65dc90e20556987c75f6fc3e8269e4e783406ede
Yara Amr-Allah <yara.amrallah.94@gmail.com>
2020-08-08 02:50:03 +0200

    Main file
    Default directory + first user interface

--- DBSearchEngine.sh
+++ DBSearchEngine.sh
@@ -1,1 +1,88 @@
+#!/bin/bash
 
+# SHORTCUTS
+
+# Working Space Shortcut
+
+mySpace=$(awk -F: '{if(NR == 2){print $2}}' ./configFile.txt)
+
+# Working Directory Shortcut
+
+myDirectory=$(awk -F: '{if(NR == 1){print $2}}' ./configFile.txt)
+# ====================================================================
+# CREAT DEAFULT DIRECTORY
+
+if [ ! -d "./Databases" ]
+then
+mkdir Databases
+fi
+# ====================================================================
+# SET DEFAULT WORKING SPACE
+
+echo "Do you want to use: $mySpace as your default space?"
+echo "Y/N/Press any other key to exit"
+read answer
+clear
+
+flag=1
+
+if [[ $answer =~ ^[Y,y] ]]
+then
+echo "The current directory is: $mySpace"
+cd $mySpace
+sleep 2
+clear
+elif [[ $answer =~ ^[N,n] ]]
+then
+while [ $flag -eq 1 ]
+do
+	echo "Enter the new absolute directory: "
+	read answer
+	clear
+	if [ -d $answer ]
+	then
+	flag=0
+	awk -i inplace -F: -v var="$answer" '{if(NR==2){gsub($2,var)};{print $0}}' ./configFile.txt
+	echo "Current Working Space is: $answer"
+	cd $answer
+	pwd
+	sleep 2
+	clear
+	else
+	echo "Wrong Directory !"
+	sleep 1
+	clear
+	fi
+done
+else
+clear
+exit
+fi
+# ====================================================================
+# USER INTERFACE
+
+echo "Main Menu:"
+IFS=""
+choices=(
+"Create new DB"
+"Delete DB"
+"Open DB"
+"List existing DBs"
+"Exit")
+PS3='Enter your choice: '
+select choice in  ${choices[@]}
+do
+case $REPLY in
+	1) echo "$choice ... "
+	   break;;
+	2) echo "$choice ... "
+	  break;;
+	3) echo "$choice ... "
+          break;;
+	4) echo "$choice ... "
+          break;;
+	5) echo "$choice ... "
+          break;;
+	*) echo "Wrong choice, try again"
+esac
+done
