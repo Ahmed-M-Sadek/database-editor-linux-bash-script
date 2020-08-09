13ae7f61c816949456bb338b54b4a298ee1d99b9
Strykar1 <ahmedms.ftw@gmail.com>
2020-08-09 02:49:32 +0200

    feat: added the ability to create a new table file

---
+++ createTable.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#sub menu choices
+
+choices=(
+"Integer"
+"String")
+
+clear
+echo "Enter a name for the new table"
+read tableName
+sleep 0.5
+clear
+
+touch $tableName
+
+echo "Enter the number of columns"
+read cnumber
+
+while [[ $cnumber =~ '!(^[0-9]+)' || $cnumber -lt 1 ]]
+do
+	echo "PLease enter a number of columns"
+	read cnumber
+done
+echo $cnumber > $tableName
+
+echo "Enter the primary key for the database"
+read localPK
+select choice in ${choices[@]}
+do
+case $REPLY in
+        1) localtype="n"
+          break;;
+        2) localtype="s"
+          break;;
+	*) echo "Wrong choice"
+esac
+done
+
+echo -n "$localtype" >> $tableName
+fields+="$localPK"
+
+
+for (( i=2; i<=$cnumber; i++ ))
+do
+	echo "Enter field name"
+	read localName
+	echo "Enter field type for column $localName:"
+	select choice in ${choices[@]}
+	do
+	case $REPLY in
+		1) localtype="n"
+		  break;;
+		2) localtype="s"
+		  break;;
+		*) echo "Wrong choice, choose again"
+	esac
+	done
+
+	echo -n ":$localtype" >> $tableName
+	fields+=":$localName"
+done
+
+echo >> $tableName
+echo $fields >> $tableName
+
